560d1d544c80316df3ff7189dcefbfc7ca1335b2
nativeforest <andressdark@gmail.com>
2022-09-30 18:50:48 -0500

    folder structure,configs and login via google test

---
+++ google.d.ts
@@ -0,0 +1,7 @@
+exports 
+/// <reference types="google-one-tap" />
+/// <reference types="google.accounts" />
+
+declare global {
+  const google: typeof import('google-one-tap');
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
+  const handleCallbackResponse = (response: google.accounts.id.CredentialResponse) => {
+    console.log("Encoded JWT",response.credential);
+  };
+  useEffect(() => {
+    /* global google */
+   google.accounts.id.initialize({
+      client_id: '282669505556-8hn58876v5ktiqd998vmlgki3n7l0pro.apps.googleusercontent.com',
+      callback: handleCallbackResponse,
+    })
+    google.accounts.id.renderButton(
+      (document.getElementById('signInDiv') as HTMLInputElement),
+      {theme: 'outline', size: 'large', type: 'standard'},
+    )
+      
+    
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div id="signInDiv"></div>
         <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
     </div>
   );
